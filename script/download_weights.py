4e3e3ed725ea822b7ed0bed44654fc908dd9a9ab
Luke Boggs <lukeboggs7@gmail.com>
2024-01-10 13:56:36 -0600

    fixy fix

--- script/download_weights.py
+++ script/download_weights.py
@@ -3,35 +3,40 @@
 # internet, which will take a long time.
 
 import torch
-import os
+import sys
 from diffusers import AutoencoderKL, DiffusionPipeline
 
-better_vae = AutoencoderKL.from_pretrained(
-    "madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16
-)
+def main(token):
+    better_vae = AutoencoderKL.from_pretrained(
+        "madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16
+    )
 
-pipe = DiffusionPipeline.from_pretrained(
-    "SG161222/RealVisXL_V3.0",
-    vae=better_vae,
-    torch_dtype=torch.float16,
-    use_safetensors=True,
-    variant="fp16",
-)
+    pipe = DiffusionPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-xl-base-1.0",
+        vae=better_vae,
+        torch_dtype=torch.float16,
+        use_safetensors=True,
+        variant="fp16",
+    )
 
-pipe.load_lora_weights("bawgz/dripfusion", weight_name="drip_glasses.safetensors", adapter_name="DRIP", token=os.getenv("token"))
+    pipe.load_lora_weights("bawgz/dripfusion", weight_name="drip_glasses.safetensors", adapter_name="DRIP", token=token)
 
-pipe.set_adapters(["DRIP"], adapter_weights=[0.6])
+    pipe.set_adapters(["DRIP"], adapter_weights=[0.6])
 
-pipe.fuse_lora()
+    pipe.fuse_lora()
 
-pipe.save_pretrained("./sdxl-cache", safe_serialization=True)
+    pipe.save_pretrained("./sdxl-cache", safe_serialization=True)
 
-pipe = DiffusionPipeline.from_pretrained(
-    "stabilityai/stable-diffusion-xl-refiner-1.0",
-    torch_dtype=torch.float16,
-    use_safetensors=True,
-    variant="fp16",
-)
+    pipe = DiffusionPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-xl-refiner-1.0",
+        torch_dtype=torch.float16,
+        use_safetensors=True,
+        variant="fp16",
+    )
 
-# TODO - we don't need to save all of this and in fact should save just the unet, tokenizer, and config.
-pipe.save_pretrained("./refiner-cache", safe_serialization=True)
+    # TODO - we don't need to save all of this and in fact should save just the unet, tokenizer, and config.
+    pipe.save_pretrained("./refiner-cache", safe_serialization=True)
+
+if __name__ == "__main__":
+    hf_token = sys.argv[1]
+    main(hf_token)
