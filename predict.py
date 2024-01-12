f57298259fc1fb8470be441d10bf35a4eb1d31a8
Luke Boggs <lukeboggs7@gmail.com>
2024-01-12 10:08:59 -0600

    download the weights in the setup to make the image smaller and speed up startup

--- predict.py
+++ predict.py
@@ -1,5 +1,4 @@
 from typing import List, Optional
-import json
 import os
 import time
 import subprocess
@@ -15,6 +14,7 @@ from diffusers import (
     EulerAncestralDiscreteScheduler,
     EulerDiscreteScheduler,
     HeunDiscreteScheduler,
+    AutoencoderKL,
     PNDMScheduler
 )
 
@@ -50,110 +50,36 @@ def download_weights(url, dest):
     print("downloading took: ", time.time() - start)
 
 class Predictor(BasePredictor):
-    def load_trained_weights(self, pipe):
-        from no_init import no_init_or_tensor
-
-        # weights can be a URLPath, which behaves in unexpected ways
-        # weights = str(weights)
-        # if self.tuned_weights == weights:
-        #     print("skipping loading .. weights already loaded")
-        #     return
-
-        # self.tuned_weights = weights
-
-        local_weights_cache = TRAINED_MODEL_LOCATION
-
-        # load UNET
-        print("Loading fine-tuned model")
-        self.is_lora = False
-
-        maybe_unet_path = os.path.join(local_weights_cache, "unet.safetensors")
-        if not os.path.exists(maybe_unet_path):
-            print("Does not have Unet. assume we are using LoRA")
-            self.is_lora = True
-
-        if not self.is_lora:
-            print("Loading Unet")
-
-            new_unet_params = load_file(
-                os.path.join(local_weights_cache, "unet.safetensors")
-            )
-            # this should return _IncompatibleKeys(missing_keys=[...], unexpected_keys=[])
-            pipe.unet.load_state_dict(new_unet_params, strict=False)
-
-        else:
-            print("Loading Unet LoRA")
-
-            unet = pipe.unet
-
-            tensors = load_file(os.path.join(local_weights_cache, "lora.safetensors"))
-
-            unet_lora_attn_procs = {}
-            name_rank_map = {}
-            for tk, tv in tensors.items():
-                # up is N, d
-                if tk.endswith("up.weight"):
-                    proc_name = ".".join(tk.split(".")[:-3])
-                    r = tv.shape[1]
-                    name_rank_map[proc_name] = r
-
-            for name, attn_processor in unet.attn_processors.items():
-                cross_attention_dim = (
-                    None
-                    if name.endswith("attn1.processor")
-                    else unet.config.cross_attention_dim
-                )
-                if name.startswith("mid_block"):
-                    hidden_size = unet.config.block_out_channels[-1]
-                elif name.startswith("up_blocks"):
-                    block_id = int(name[len("up_blocks.")])
-                    hidden_size = list(reversed(unet.config.block_out_channels))[
-                        block_id
-                    ]
-                elif name.startswith("down_blocks"):
-                    block_id = int(name[len("down_blocks.")])
-                    hidden_size = unet.config.block_out_channels[block_id]
-                with no_init_or_tensor():
-                    module = LoRAAttnProcessor2_0(
-                        hidden_size=hidden_size,
-                        cross_attention_dim=cross_attention_dim,
-                        rank=name_rank_map[name],
-                    )
-                unet_lora_attn_procs[name] = module.to("cuda", non_blocking=True)
-
-            unet.set_attn_processor(unet_lora_attn_procs)
-            unet.load_state_dict(tensors, strict=False)
-
-        # load text
-        handler = TokenEmbeddingsHandler(
-            [pipe.text_encoder, pipe.text_encoder_2], [pipe.tokenizer, pipe.tokenizer_2]
-        )
-        handler.load_embeddings(os.path.join(local_weights_cache, "embeddings.pti"))
-
-        # load params
-        with open(os.path.join(local_weights_cache, "special_params.json"), "r") as f:
-            params = json.load(f)
-        self.token_map = params
-
-        self.tuned_model = True
-
-
     def setup(self, weights: Optional[Path] = None):
         print("weights: ", weights)
         
         if str(weights) == "weights":
             weights = None
 
-        if not os.path.exists(SDXL_MODEL_CACHE):
-            download_weights(SDXL_URL, SDXL_MODEL_CACHE)
-
         print("Loading sdxl txt2img pipeline...")
-        self.pipe = DiffusionPipeline.from_pretrained(
-            SDXL_MODEL_CACHE,
-            torch_dtype=torch.float16,
-            use_safetensors=True,
-            variant="fp16"
-        ).to("cuda")
+
+        if not os.path.exists(SDXL_MODEL_CACHE):
+            better_vae = AutoencoderKL.from_pretrained(
+                "madebyollin/sdxl-vae-fp16-fix",
+                torch_dtype=torch.float16
+            )
+
+            self.pipe = DiffusionPipeline.from_pretrained(
+                "stabilityai/stable-diffusion-xl-base-1.0",
+                vae=better_vae,
+                torch_dtype=torch.float16,
+                use_safetensors=True,
+                variant="fp16",
+            )
+
+            self.pipe.save_pretrained("./sdxl-cache", safe_serialization=True)
+        else:
+            self.pipe = DiffusionPipeline.from_pretrained(
+                SDXL_MODEL_CACHE,
+                torch_dtype=torch.float16,
+                use_safetensors=True,
+                variant="fp16"
+            )
 
         self.pipe.load_lora_weights("./", weight_name="drip_glasses.safetensors", adapter_name="DRIP")
 
@@ -167,8 +93,6 @@ class Predictor(BasePredictor):
             print("Loading fine-tuned model")
             state_dict = load_file(os.path.join(TRAINED_MODEL_LOCATION, "embeddings.pti"))
 
-            print("State dict[\"text_encoders_0\"]", state_dict["text_encoders_0"])
-            print("State dict[\"text_encoders_1\"]", state_dict["text_encoders_1"])
             # notice we load the tokens <s0><s1>, as "TOK" as only a place-holder and training was performed using the new initialized tokens - <s0><s1>
             # load embeddings of text_encoder 1 (CLIP ViT-L/14)
             self.pipe.load_textual_inversion(state_dict["text_encoders_0"], token=["<s0>", "<s1>"], text_encoder=self.pipe.text_encoder, tokenizer=self.pipe.tokenizer)
@@ -176,31 +100,37 @@ class Predictor(BasePredictor):
             self.pipe.load_textual_inversion(state_dict["text_encoders_1"], token=["<s0>", "<s1>"], text_encoder=self.pipe.text_encoder_2, tokenizer=self.pipe.tokenizer_2)
             self.pipe.load_lora_weights(TRAINED_MODEL_LOCATION, weight_name="lora.safetensors", adapter_name="TOK")
 
-            #self.load_trained_weights(self.pipe)
-
-        # self.pipe.load_lora_weights("./", weight_name="drip_glasses.safetensors", adapter_name="TOK")
-        # self.pipe.load_lora_weights("./trained-model/", weight_name="lora.safetensors", adapter_name="LUK")
-
-        # self.trained_model = os.path.exists("./trained-model/")
-        # if self.trained_model:
-        #     self.pipe.load_lora_weights("./trained-model/", weight_name="lora.safetensors", adapter_name="LUK")
-
         print("Loading SDXL refiner pipeline...")
         # FIXME(ja): should the vae/text_encoder_2 be loaded from SDXL always?
         #            - in the case of fine-tuned SDXL should we still?
         # FIXME(ja): if the answer to above is use VAE/Text_Encoder_2 from fine-tune
         #            what does this imply about lora + refiner? does the refiner need to know about
 
-
         print("Loading refiner pipeline...")
-        self.refiner = DiffusionPipeline.from_pretrained(
-            REFINER_MODEL_CACHE,
-            text_encoder_2=self.pipe.text_encoder_2,
-            vae=self.pipe.vae,
-            torch_dtype=torch.float16,
-            use_safetensors=True,
-            variant="fp16",
-        ).to("cuda")
+
+        if os.path.exists(REFINER_MODEL_CACHE):
+            self.refiner = DiffusionPipeline.from_pretrained(
+                REFINER_MODEL_CACHE,
+                text_encoder_2=self.pipe.text_encoder_2,
+                vae=self.pipe.vae,
+                torch_dtype=torch.float16,
+                use_safetensors=True,
+                variant="fp16",
+            )           
+        else:
+            self.refiner = DiffusionPipeline.from_pretrained(
+                "stabilityai/stable-diffusion-xl-refiner-1.0",
+                text_encoder_2=self.pipe.text_encoder_2,
+                vae=self.pipe.vae,
+                torch_dtype=torch.float16,
+                use_safetensors=True,
+                variant="fp16",
+            )
+
+            self.refiner.save_pretrained("./refiner-cache", safe_serialization=True)
+        
+        self.refiner.to("cuda")
+
 
         # FIXME: should I load lora weights to the refiner? Below does not work
         # print("setting refiner adapters")
@@ -243,6 +173,14 @@ class Predictor(BasePredictor):
             le=1.0,
             default=0.6,
         ),
+        replicate_weights: str = Input(
+            description="Replicate LoRA weights to use. Leave blank to use the default weights.",
+            default=None,
+        ),
+        custom_weights: str = Input(
+            description="Replicate LoRA weights to use. Leave blank to use the default weights.",
+            default=None,
+        ),
         refine: str = Input(
             description="Which refine style to use",
             choices=["no_refiner", "expert_ensemble_refiner", "base_image_refiner"],
@@ -268,9 +206,19 @@ class Predictor(BasePredictor):
             seed = int.from_bytes(os.urandom(2), "big")
         print(f"Using seed: {seed}")
 
-        # if self.trained_model:
-        #     print("using two loras")
-        #     self.pipe.set_adapters(["TOK", "LUK"], adapter_weights=[lora_scale_base, lora_scale_custom])
+        if self.replicate_weights:
+            print("downloading weights")
+            self.trained_model = True
+            download_weights(replicate_weights, TRAINED_MODEL_LOCATION)
+            
+            state_dict = load_file(os.path.join(TRAINED_MODEL_LOCATION, "embeddings.pti"))
+
+            # notice we load the tokens <s0><s1>, as "TOK" as only a place-holder and training was performed using the new initialized tokens - <s0><s1>
+            # load embeddings of text_encoder 1 (CLIP ViT-L/14)
+            self.pipe.load_textual_inversion(state_dict["text_encoders_0"], token=["<s0>", "<s1>"], text_encoder=self.pipe.text_encoder, tokenizer=self.pipe.tokenizer)
+            # load embeddings of text_encoder 2 (CLIP ViT-G/14)
+            self.pipe.load_textual_inversion(state_dict["text_encoders_1"], token=["<s0>", "<s1>"], text_encoder=self.pipe.text_encoder_2, tokenizer=self.pipe.tokenizer_2)
+            self.pipe.load_lora_weights(TRAINED_MODEL_LOCATION, weight_name="lora.safetensors", adapter_name="TOK")
 
 
         if self.trained_model:
--- script/download_weights.py
+++ script/download_weights.py
@@ -9,35 +9,6 @@ from huggingface_hub import hf_hub_download
 
 def main(token):
     hf_hub_download(repo_id="bawgz/dripfusion", filename="drip_glasses.safetensors", repo_type="model", local_dir="./", local_dir_use_symlinks=False, use_auth_token=token)
-    better_vae = AutoencoderKL.from_pretrained(
-        "madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16
-    )
-
-    pipe = DiffusionPipeline.from_pretrained(
-        "stabilityai/stable-diffusion-xl-base-1.0",
-        vae=better_vae,
-        torch_dtype=torch.float16,
-        use_safetensors=True,
-        variant="fp16",
-    )
-
-    pipe.save_pretrained("./sdxl-cache", safe_serialization=True)
-
-    # pipe.load_lora_weights("bawgz/dripfusion", weight_name="drip_glasses.safetensors", adapter_name="DRIP", use_auth_token=token)
-
-    # pipe.set_adapters(["DRIP"], adapter_weights=[0.6])
-
-    # pipe.fuse_lora()
-
-    pipe = DiffusionPipeline.from_pretrained(
-        "stabilityai/stable-diffusion-xl-refiner-1.0",
-        torch_dtype=torch.float16,
-        use_safetensors=True,
-        variant="fp16",
-    )
-
-    # TODO - we don't need to save all of this and in fact should save just the unet, tokenizer, and config.
-    pipe.save_pretrained("./refiner-cache", safe_serialization=True)
 
 if __name__ == "__main__":
     hf_token = sys.argv[1]
