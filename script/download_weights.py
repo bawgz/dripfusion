315b22e06a247372969f7da2a789dd01da5bc0dd
Luke Boggs <lukeboggs7@gmail.com>
2024-01-15 14:36:27 -0600

    update download weights script to download new base model

--- script/download_weights.py
+++ script/download_weights.py
@@ -8,7 +8,17 @@ from diffusers import AutoencoderKL, DiffusionPipeline
 from huggingface_hub import hf_hub_download
 
 def main(token):
-    hf_hub_download(repo_id="bawgz/dripfusion", filename="drip_glasses.safetensors", repo_type="model", local_dir="./", local_dir_use_symlinks=False, use_auth_token=token)
+    # hf_hub_download(repo_id="bawgz/dripfusion", filename="drip_glasses.safetensors", repo_type="model", local_dir="./", local_dir_use_symlinks=False, use_auth_token=token)
+
+    pipe = DiffusionPipeline.from_pretrained(
+        "bawgz/dripfusion-base",
+        torch_dtype=torch.float16,
+        use_safetensors=True,
+        variant="fp16",
+        token=token,
+    )
+
+    pipe.save_pretrained("./sdxl-cache", variant="fp16")
 
 if __name__ == "__main__":
     hf_token = sys.argv[1]
