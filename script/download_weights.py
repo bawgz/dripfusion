397f774d4050333a7dcebba2b12be59ec59df47c
Luke Boggs <lukeboggs7@gmail.com>
2024-01-16 14:11:04 -0600

    ok realvis just straight up not working :thumbsup:

--- script/download_weights.py
+++ script/download_weights.py
@@ -11,7 +11,7 @@ def main(token):
     hf_hub_download(repo_id="bawgz/dripglasses_lora", filename="pit_viper_sunglasses.safetensors", repo_type="model", local_dir="./", local_dir_use_symlinks=False, use_auth_token=token)
 
     pipe = DiffusionPipeline.from_pretrained(
-        pretrained_model_name_or_path="SG161222/RealVisXL_V3.0",
+        pretrained_model_name_or_path="stabilityai/stable-diffusion-xl-base-1.0",
         torch_dtype=torch.float16,
         use_safetensors=True,
         variant="fp16",
